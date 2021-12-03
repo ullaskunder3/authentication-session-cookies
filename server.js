9763d89829062ec4087b042ef9b1c8e59adf0d3f
ullas <ullaskunder3@gmail.com>
2021-12-03 13:41:15 +0530

    testing session ✨

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const app = express();
+const session = require('express-session');
 
+const app = express();
 const PORT = 5000;
 
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false
+}))
+
 app.get('/', (req,res)=>{
+    req.session.isAuth = true;
+    console.log(req.session);
+    console.log(req.session.id);
     res.send("Welcome to the session")
 })
 
