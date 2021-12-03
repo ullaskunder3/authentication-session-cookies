cef8ec695d3915e9d2c18e589bad80b13adf6f2c
ullas <ullaskunder3@gmail.com>
2021-12-03 14:01:22 +0530

    session req stored in DB

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const session = require('express-session');
+const mongodbSession = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
 
+mongoose.connect('mongodb://localhost:27017/sessions', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() =>{
+    console.log('Mongodb is connected...')
+}).catch(error =>{
+    console.log('Failed to connect', error)
+})
+const store = new mongodbSession({
+    uri: 'mongodb://localhost:27017/sessions',
+    collection: 'currentSession'
+})
+
 app.use(session({
     secret: 'secret key',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store,
 }))
 
 app.get('/', (req,res)=>{
